55b3a9e05bb79c23917e92109e397b2543edb7c2
reatomy <under1210@gmail.com>
2020-08-05 17:34:51 +0900

    YH: added - models.py

---
+++ Codes/models.py
@@ -0,0 +1,11 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+
+class SiameseConvNet(nn.Module):
+    def __init__(self):
+        pass
+
+    def forward(self, img1, img2, label):
+        pass
