525b05722d0d60f7e15149927ad7efc483d5f322
reatomy <under1210@gmail.com>
2020-08-05 17:35:12 +0900

    YH: edited utils.py - balanced sampling

--- Codes/utils.py
+++ Codes/utils.py
@@ -27,7 +27,7 @@ class DataManager(object):
         print("\t", list(self.valid_paths.keys())[:5])
         print("ALPHABET IN EVALUATION", ":", len(list(self.eval_paths.keys())))
         print("\t", list(self.eval_paths.keys())[:5])
-        
+
 
         self.sample_drawers()
 
@@ -48,7 +48,7 @@ class DataManager(object):
                             path_dict[alphabet_name][character_name].append(char_img_path)
 
         return path_dict
-    
+
     def sample_drawers(self, train_cnt = 12, valid_cnt = 4):
         random.seed(self.seed)
         t = random.sample(range(1, 21), train_cnt)
@@ -79,17 +79,38 @@ class VerificationDataset(Dataset):
         self.data_pairs = self.sample_pairs()
         self.pair_statistics()
         self.trans = transforms.ToTensor()
-    
+
     def __len__(self):
         return len(self.data_pairs)
-    
+
     def __getitem__(self, idx):
         pair = self.data_pairs[idx]
+
+        if type(pair) == tuple:
+            # Single sample
+
+            images = [self.trans(Image.open(p)) for p in pair[:2]]
+            label = torch.from_numpy(np.array([int(pair[2])], dtype=np.float32))
+
+            return (images[0], images[1], label)
+
+        else:
+            res = []
+
+            for p in pair:
+                images = [self.trans(Image.open(_p)) for _p in p[:2]]
+                label = torch.from_numpy(np.array([int(p[2])], dtype=np.float32))
+
+                res.append((images[0], images[1], label))
+
+            return res
+
+
         # images = [io.imread(p) for p in pair[:2]]
-        images = [self.trans(Image.open(p)) for p in pair[:2]]
-        label = torch.from_numpy(np.array([int(pair[-1])], dtype=np.float32))
-        return images[0], images[1], label
-    
+        # images = [self.trans(Image.open(p)) for p in pair[:2]]
+        # label = torch.from_numpy(np.array([int(pair[-1])], dtype=np.float32))
+        # return images[0], images[1], label
+
     def generate_pairs(self, total_examples):
         # Training example(alphabet pair) generator
         # Generates unique pair
@@ -101,7 +122,7 @@ class VerificationDataset(Dataset):
         pair_indices = random.sample(range(total_example_size), 2)
         x = pair_indices[0]
         y = pair_indices[1]
-        
+
         x_alp = total_examples[x][1]
         y_alp = total_examples[y][1]
         while True:
@@ -119,7 +140,7 @@ class VerificationDataset(Dataset):
                 pair_indices = random.sample(range(total_example_size), 2)
                 x = pair_indices[0]
                 y = pair_indices[1]
-                
+
                 x_alp = total_examples[x][1]
                 y_alp = total_examples[y][1]
 
@@ -133,33 +154,68 @@ class VerificationDataset(Dataset):
                     if i_path.split(".")[-2][-2:] in self.drawers:
                         total_examples.append((i_path, alp, "_".join((alp, c))))
 
-        
+        total_classes = []
+        for alp in self.path_dict.keys():
+            for c in self.path_dict[alp].keys():
+                total_classes.append("//".join((alp, c)))
+
         # g = gencoordinates(0, len(total_examples)-1)
         g = self.generate_pairs(total_examples)
 
         for idx in range(self.sample_size):
-            idx_pair = next(g)
+
+            label = idx % 2
+
+            # TODO: UNIFORM NUMBER PROBLEM
+
+            if label == 1:
+                # Same class example
+                c = random.choice(total_classes)
+                i_path_list = self.path_dict[c.split("//")[0]][c.split("//")[1]]
+                i_path_list = [i for i in i_path_list if i.split(".")[-2][-2:] in self.drawers]
+                i_sample = random.sample(i_path_list, 2)
+                i_sample.append(label)
+
+                res.append(tuple(i_sample))
+
+            else:
+                # different class example
+
+                cs = random.sample(total_classes, 2)
+                i_sample = []
+                for c in cs:
+                    i_path_list = self.path_dict[c.split("//")[0]][c.split("//")[1]]
+                    i_path_list = [i for i in i_path_list if i.split(".")[-2][-2:] in self.drawers]
+                    i_item = random.choice(i_path_list)
+                    i_sample.append(i_item)
+                i_sample.append(label)
+
+                res.append(tuple(i_sample))
+
+
+            # idx_pair = next(g)
             # print(idx_pair)
             # print(total_examples[idx_pair[0]][0], total_examples[idx_pair[0]][1], total_examples[idx_pair[0]][2])
             # print(total_examples[idx_pair[1]][0], total_examples[idx_pair[1]][1], total_examples[idx_pair[1]][2])
             # print(total_examples[idx_pair[0]][2] == total_examples[idx_pair[1]][2])
             # raise
 
-            res.append((total_examples[idx_pair[0]][0], total_examples[idx_pair[1]][0], total_examples[idx_pair[0]][2] == total_examples[idx_pair[1]][2]))
+            # res.append((total_examples[idx_pair[0]][0], total_examples[idx_pair[1]][0], total_examples[idx_pair[0]][2] == total_examples[idx_pair[1]][2]))
 
             if (idx+1) % 10000 == 0:
                 print("\t", (idx+1), "th pair was generated:")
-                print("\t", total_examples[idx_pair[0]][0], total_examples[idx_pair[0]][1], total_examples[idx_pair[0]][2])
-                print("\t", total_examples[idx_pair[1]][0], total_examples[idx_pair[1]][1], total_examples[idx_pair[1]][2])
+                print("\t", res[idx][0], res[idx][1], res[idx][2])
+                # print("\t", total_examples[idx_pair[0]][0], total_examples[idx_pair[0]][1], total_examples[idx_pair[0]][2])
+                # print("\t", total_examples[idx_pair[1]][0], total_examples[idx_pair[1]][1], total_examples[idx_pair[1]][2])
 
         return res
-    
+
     def pair_statistics(self):
         alp_cnts = {k: 0 for k in self.path_dict.keys()}
         same_cnt = 0
 
         for item in self.data_pairs:
-            
+
             # x_alp = item[0].split('\\')[0].split('/')[-1]
             x_alp = Path(item[0]).parent.parent.name
             alp_cnts[x_alp] += 1
@@ -168,7 +224,7 @@ class VerificationDataset(Dataset):
             alp_cnts[y_alp] += 1
             if item[2]:
                 same_cnt += 1
-        
+
         print("=================================")
         print("STATISTICS OF GENERATED EXAMPLES:")
         print("\tTOTAL EXAMPLE:", len(self.data_pairs))
@@ -177,12 +233,13 @@ class VerificationDataset(Dataset):
         print('\t', "CNT OF SAME CLASS:", same_cnt)
 
 class OneshotDataset(Dataset):
-    def __init__(self, path_dict, n_way):
+    def __init__(self, path_dict, drawers, n_way):
         self.path_dict = path_dict
+        self.drawers = drawers
         self.n_way = n_way
         self.data = self.make_trials()
         self.trans = transforms.ToTensor()
-    
+
     def make_trials(self):
         res = []
         for alp in self.path_dict.keys():
@@ -192,27 +249,50 @@ class OneshotDataset(Dataset):
 
                 a_images = [self.path_dict[alp][c][drawers[0]] for c in chars]
                 b_images = [self.path_dict[alp][c][drawers[1]] for c in chars]
-                
+
                 for a_idx, a_image in enumerate(a_images):
                     res.append((a_image, b_images, a_idx))
-        
+
         return res
 
-    
+
     def __len__(self):
         return len(self.data)
-    
+
     def __getitem__(self, idx):
         trial = self.data[idx]
 
-        # a_image = io.imread(trial[0])
-        a_image = self.trans(Image.open(trial[0]))
-        # b_images = [io.imread(b) for b in trial[1]]
-        b_images = [self.trans(Image.open(b)) for b in trial[1]]
-        label = torch.from_numpy(np.array([trial[2]], dtype=np.float32))
-        
-        return a_image, b_images, label
-    
+        if(type(trial) == tuple):
+            # Single sample
+
+            a_image = self.trans(Image.open(trial[0]))
+            b_images = [self.trans(Image.open(b)) for b in trial[1]]
+            label = torch.from_numpy(np.array([trial[2]], dtype=np.float32))
+
+            return (a_image, b_images, label)
+
+        else:
+            res = []
+            for t in trial:
+                a_image = self.trans(Image.open(t[0]))
+                b_images = [self.trans(Image.open(b)) for b in t[1]]
+                label = torch.from_numpy(np.array([t[2]], dtype=np.float32))
+
+                res.append((a_image, b_images, label))
+
+            return res
+        #
+        #
+        # pdb.set_trace()
+        #
+        # # a_image = io.imread(trial[0])
+        # a_image = self.trans(Image.open(trial[0]))
+        # # b_images = [io.imread(b) for b in trial[1]]
+        # b_images = [self.trans(Image.open(b)) for b in trial[1]]
+        # label = torch.from_numpy(np.array([trial[2]], dtype=np.float32))
+        #
+        # return a_image, b_images, label
+
 
 def int_list_to_strs(l):
     # Drawer indices to list of string
@@ -221,13 +301,13 @@ def int_list_to_strs(l):
 
 
 if __name__ == "__main__":
-    bg_dir = "../data/python/images_background/images_background/"
-    eval_dir = "../data/python/images_evaluation/images_evaluation/"
+    bg_dir = "../data/python/images_background/"
+    eval_dir = "../data/python/images_evaluation/"
     seed = 10
     dm = DataManager(bg_dir = bg_dir, eval_dir = eval_dir, seed = seed)
 
     train_vd = VerificationDataset(path_dict = dm.bg_paths, drawers = dm.train_drawers, sample_size = 30000)
-    print("EXAMPLE:")
-    print(train_vd[0])
+    print(train_vd[:2])
 
-    od = OneshotDataset(path_dict = dm.eval_paths, n_way = 20)
+    od = OneshotDataset(path_dict = dm.eval_paths, drawers = dm.test_drawers, n_way = 20)
+    # print(od[:2])
